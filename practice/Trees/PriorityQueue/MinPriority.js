4457eeca27ac7847237930a13b17e0144b63dc5c
dantedxwolf <teacher.martin@gmail.com>
2020-12-10 12:53:16 +0600

    Priority Queues implemented

---
+++ practice/Trees/PriorityQueue/MinPriority.js
@@ -0,0 +1,97 @@
+/* insertion O(log n)
+   removal O(log n)
+   search O(n),
+   any index n in the array, left child @ 2n + 1, right child @2n+2,
+   if child @ n, parent @ Math.floor((n-1)/2)
+*/
+
+
+class PriorityQueueMin{
+    constructor(){
+        this.values = [];
+    }
+    bubbleUp(){
+        let elementIndex = this.values.length - 1;
+        let element = this.values[elementIndex];
+        while(elementIndex > 0){
+            let parentIndex = Math.floor((elementIndex - 1)/2);
+            let parentElement = this.values[parentIndex];
+            
+            if(parentElement.priority <= element.priority) break;
+
+            this.values[parentIndex] = element;
+            this.values[elementIndex] = parentElement;
+            elementIndex = parentIndex;
+
+        }
+    }
+    enqueue(val,priority){
+        if(priority === undefined || val === undefined) return false;
+        this.values = this.values.filter(obj => obj.val !== val);
+        this.values.push({val,priority});
+        this.bubbleUp();
+        return true;
+    }
+    checkPriority(val){
+        for(let obj of this.values){
+            if(obj.val === val){
+                return obj.priority;
+            }
+        }
+        return undefined;
+    }
+
+    /* we first extract the min from minheap
+    and then fill the empty node with last added element,
+    which will be already down in extract min
+    function */
+    sinkDown(){
+        let index = 0
+        let length = this.values.length;
+        let element = this.values[0];
+
+        while(true){
+            let leftChildIndex = (2 * index) + 1;
+            let rightChildIndex = (2 * index) + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+            if(leftChildIndex < length ){
+                leftChild = this.values[leftChildIndex];
+                if(leftChild.priority < element.priority){
+                    swap = leftChildIndex;
+                }
+            }
+
+            if(rightChildIndex < length ){
+                rightChild = this.values[rightChildIndex];
+                if(
+                    ((swap !== null) && (rightChild.priority < leftChild.priority)) ||
+                    ((swap === null) && rightChild.priority < element.priority)
+                ){
+                    swap = rightChildIndex;
+                }
+            }
+
+            if(swap === null) break;
+
+            this.values[index] = this.values[swap];
+            this.values[swap] = element;
+            index = swap;
+        }
+    }
+
+    dequeueMin(){
+        let min = this.values[0];
+        let end = this.values.pop();
+        if(this.values.length > 0){
+            this.values[0] = end;
+            this.sinkDown();
+        }
+        return min;
+    }
+}
+
+
+
+module.exports = PriorityQueueMin;
